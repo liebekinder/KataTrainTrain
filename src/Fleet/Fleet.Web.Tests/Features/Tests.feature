8945640a6f1b19389e60873287dc19250032c90e
Christophe Moinard <cmoinard@lucca.fr>
2022-07-26 11:07:28 +0200

    GetByIdAsync avec validations

--- src/Fleet/Fleet.Web.Tests/Features/Tests.feature
+++ src/Fleet/Fleet.Web.Tests/Features/Tests.feature
@@ -1,7 +1,7 @@
 ﻿@Tests
 Feature: Tests
 
-    Scenario: NotFound with inexisting prorata rule
+    Scenario: GET should succeed
         When http GET /fleet
         Then the http status is 200
         And the http content is Success!!1
--- src/Fleet/Fleet.Web/Controllers/FleetController.cs
+++ src/Fleet/Fleet.Web/Controllers/FleetController.cs
@@ -14,4 +14,16 @@ public class FleetController : ControllerBase
         await Task.CompletedTask;
         return Ok("Success!!1");
     }
+    
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<string>> GetByIdAsync(int id)
+    {
+        if (!ModelState.IsValid)
+        {
+            return ValidationProblem(ModelState);
+
+        }
+        await Task.CompletedTask;
+        return Ok("Success!!1");
+    }
 }
