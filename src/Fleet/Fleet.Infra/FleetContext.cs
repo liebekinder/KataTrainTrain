67b61861cfda5c822fd7e02219f1a95f2ce3a56b
Christophe Moinard <cmoinard@lucca.fr>
2022-09-07 18:09:46 +0200

    Configuration de base pour EF

---
+++ src/Fleet/Fleet.Infra/FleetContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Fleet.Infra;
+
+public class FleetContext : DbContext
+{
+    public FleetContext(DbContextOptions<FleetContext> options)
+        : base(options)
+    {
+    }
+}
---
+++ src/Fleet/Fleet.Infra/FleetContextFactory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Fleet.Infra;
+
+public class FleetContextFactory : IDesignTimeDbContextFactory<FleetContext>
+{
+    public FleetContext CreateDbContext(string[] args)
+    {
+        var path = Path.Combine(
+            Directory.GetCurrentDirectory(),
+            @"..\..\TrainTrain.Web"
+        );
+        Console.WriteLine(path);
+
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(
+                path)
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var connectionString = configuration.GetConnectionString("Trucs");
+        var optionsBuilder = new DbContextOptionsBuilder<FleetContext>();
+        optionsBuilder.UseSqlServer(connectionString);
+
+        return new FleetContext(optionsBuilder.Options);
+    }
+}
