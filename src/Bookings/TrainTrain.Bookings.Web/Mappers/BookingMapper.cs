57348bae1f13bdae545ecaef10c9327f95d44da2
schenais@lucca.fr <schenais@lucca.fr>
2023-03-15 17:39:59 +0100

    Fix Mapper

--- src/Bookings/TrainTrain.Bookings.Web/Mappers/BookingMapper.cs
+++ src/Bookings/TrainTrain.Bookings.Web/Mappers/BookingMapper.cs
@@ -7,18 +7,14 @@ public sealed class BookingMapper
 {
     public static BookingCandidate ToCandidate(BookingPayload payload)
     {
-        return new BookingCandidate
-        {
-            TravelId = payload.TravelId,
-            Email = payload.Email,
-            Passengers = payload.Passengers.Select(pas =>
-                new Passenger
-                {
-                    FirstName = pas.FirstName,
-                    LastName = pas.LastName,
-                    BirthDate = pas.BirthDate,
-                }).ToList()
-        };
+        return new BookingCandidate(
+            payload.TravelId,
+            payload.Email,
+            payload.Passengers.Select(pas =>
+                new Passenger(
+                    pas.FirstName,
+                    pas.LastName,
+                    pas.BirthDate)).ToList());
     }
 }
 
